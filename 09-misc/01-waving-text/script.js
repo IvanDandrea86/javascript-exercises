8fb78f1bb56e725be09022108c8049a4c1e63d90
IvanDandrea86 <ivan.d.andrea1@gmail.com>
2021-10-14 21:23:22 +0200

    text wave building

--- 09-misc/01-waving-text/script.js
+++ 09-misc/01-waving-text/script.js
@@ -10,5 +10,28 @@
 // You will have time to focus on it later.
 
 (() => {
+    const text = document.getElementById("target")
+    const strText = text.textContent;
+    var i = 10;
+    const array = Array.from(strText)
+    text.innerHTML = " "
+    for (let i = 0; i < array.length; i++) {
+        text.innerHTML += "<span>" + array[i] + "</span>"
+        document.querySelectorAll("span")
+    }
+    var spans = document.querySelectorAll("span")
+    var x = 12;
+    spans.forEach(elem => {
+        var att = document.createAttribute("style");
+        att.value = "font-size:" + x + "px";
+        elem.setAttributeNode(att);
+        if (x > 42) {
+            x = x - 4
+        } else {
+            x = x + 4;
+        }
+        console.log(x)
 
+    })
+    console.log(spans)
 })();
