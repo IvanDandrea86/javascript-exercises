60182e6b95e7de1199ae87b562650c4f541dc8c9
IvanDandrea86 <ivan.d.andrea1@gmail.com>
2021-10-17 23:07:25 +0200

    All javascript exercise finished

--- 11-fetch/05-delete/script.js
+++ 11-fetch/05-delete/script.js
@@ -10,5 +10,36 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+    document.getElementById("run").addEventListener("click",()=>{
+      let getHeroId=(elem)=>{
+        console.log("i Work")
+        let hero_id=document.getElementById("hero-id").value
+        // let hero_name=document.getElementById("hero-name").value
+        // let ego=document.getElementById("hero-alter-ego").value
+        // let powers=document.getElementById("hero-powers").value
+        // var power_split = powers.split(' ');
+        const newHero = elem[hero_id-1]
+        console.log(newHero)
+    return fetch( "http://localhost:3000/heroes", {
+        method: 'DELETE', 
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newHero),
+    })
+      .then(response =>   response.json())
+}
+
+let url="http://localhost:3000/heroes"
+    fetch(url)
+    .then(response => response.json())
+    .then(data =>getHeroId(data))
+    .catch(error => error);
+    
+})
+       
+    
+
+
+   
 })();
